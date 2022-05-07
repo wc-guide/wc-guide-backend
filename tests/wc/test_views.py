c84d35d7f678e178021320388f106b07aa102923
murthy10 <samuel.kurath@gmail.com>
2022-05-07 10:50:04 +0200

    updated tests

--- tests/wc/test_views.py
+++ tests/wc/test_views.py
@@ -4,7 +4,7 @@ import pytest
 
 from django.urls import reverse
 
-from guide.wc.models import Toilet
+from guide.wc.models import Toilet, Other
 from guide.wc.views import query_builder
 
 CONTENT_TYPE = "application/json"
@@ -14,6 +14,11 @@ def create_or_update_area(auto_login_user, area, pk='area'):
     client, user = auto_login_user()
     url = reverse('area-detail', kwargs={'pk': pk})
     return client.put(url, json.dumps(area), content_type=CONTENT_TYPE)
+
+def create_or_update_otherarea(auto_login_user, otherarea, pk='otherarea'):
+    client, user = auto_login_user()
+    url = reverse('otherarea-detail', kwargs={'pk': pk})
+    return client.put(url, json.dumps(otherarea), content_type=CONTENT_TYPE)
 
 
 @pytest.mark.django_db
@@ -38,19 +43,19 @@ def test_update_toilets_with_authenticated_client(auto_login_user, toilets_two, 
 @pytest.mark.django_db
 def test_get_empty_toilets_with_authenticated_client(auto_login_user):
     client, user = auto_login_user()
-    url = reverse('toilets-list')
+    url = reverse('others-list')
     response = client.get(url)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_get_toilets_in_bbox(auto_login_user, toilets_three):
-    response = create_or_update_area(auto_login_user=auto_login_user, area=toilets_three)
+    response = create_or_update_otherarea(auto_login_user=auto_login_user, otherarea=toilets_three)
     assert response.status_code == 201
-    assert Toilet.objects.count() == 3
+    assert Other.objects.count() == 3
 
     client, user = auto_login_user()
-    url = reverse('toilets-list')
+    url = reverse('others-list')
     response = client.get(url, {'in_bbox': "0.0,25.0,25.0,0.0"})
     content = json.loads(response.content)
     assert len(content['results']['features']) == 2
